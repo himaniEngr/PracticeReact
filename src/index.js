2832397770b9a3d82deca9bda7f35a4717da4746
Himani <himani9.engg@gmail.com>
2022-02-09 20:00:49 +0530

    conerting html to jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from './components/App';
+function getText() {
+  return "hi there";
+}
+const buttonText = { text: "click me" };
+const style = { backgroundColor: "blue", color: "white" };
+const App = () => {
+  return (
+    <div>
+      <button style={style}>{buttonText.text}</button>
+      {getText()}
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));

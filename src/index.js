75637507cd2a10eb9b3c7b5f17d13396ed73735d
Himani <himani9.engg@gmail.com>
2022-02-10 12:49:47 +0530

    creating components-naive approach

--- src/index.js
+++ src/index.js
@@ -8,9 +8,29 @@ const buttonText = { text: "click me" };
 const style = { backgroundColor: "blue", color: "white" };
 const App = () => {
   return (
-    <div>
-      <button style={style}>{buttonText.text}</button>
-      {getText()}
+    // <div>
+    //   <button style={style}>{buttonText.text}</button>
+    //   {getText()}
+    // </div>
+    <div class="ui comments">
+      <div class="comment">
+        <a class="avatar">
+          <img src="/images/avatar/small/stevie.jpg" alt="avator" />
+        </a>
+        <div class="content">
+          <a class="author">Stevie Feliciano</a>
+          <div class="metadata">
+            <div class="date">2 days ago</div>
+            <div class="rating">
+              <i class="star icon"></i>5 Faves
+            </div>
+          </div>
+          <div class="text">
+            Hey guys, I hope this example comment is helping you read this
+            documentation.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

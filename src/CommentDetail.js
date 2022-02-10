d50b3c41435a1c334fa3e6010140452b5ad39505
Himani <himani9.engg@gmail.com>
2022-02-10 18:00:33 +0530

    implementing approval card

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,25 +1,22 @@
 import react from "react";
 import reactDom from "react-dom";
-import faker from "faker";
+// import faker from "faker";
 
 const CommentDetail = (props) => {
   return (
     <div class="comment">
       <a class="avatar">
-        <img alt="avatar" src={faker.image.image()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div class="content">
         <a class="author">{props.author}</a>
         <div class="metadata">
-          <div class="date">2 days ago</div>
+          <div class="date">{props.timeAgo}</div>
           <div class="rating">
             <i class="star icon"></i>5 Faves
           </div>
         </div>
-        <div class="text">
-          Hey guys, I hope this example comment is helping you read this
-          documentation.
-        </div>
+        <div class="text">{props.postContent}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
-// import faker from "faker";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 // import App from './components/App';
 function getText() {
   return "hi there";
@@ -15,9 +16,35 @@ const App = () => {
     //   {getText()}
     // </div>
     <div class="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Peter" />
-      <CommentDetail author="Jane" />
+      <ApprovalCard>
+        <h4>warning!</h4>
+        Are you sure you want to do this?
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:00PM"
+          postContent="nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Peter"
+          timeAgo="Yesterday at 6:00PM"
+          postContent="good blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Wednesday at 4:46PM"
+          postContent="very informative blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
